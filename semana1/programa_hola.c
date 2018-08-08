2473e5776fe9e38d9c01f65269f525406ed05cd9
Romero Lopez Luis Eduardo <}>
2018-08-08 09:25:49 -0500

    agrege un programa.

---
+++ semana1/programa_hola.c
@@ -0,0 +1,13 @@
+
+/*
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ att: Luis Eduardo Romero Lopez
+*/
+#include <stdio.h>
+int main ()
+	{
+		printf("hola luis \n");
+		printf("Se que esta en algun lugar mejor \n");
+	}
+/*Termana el programa*/
+
