0c0a0f5b6cecf86a785f77137df93eb86aab2000
Romero Lopez Luis Eduardo <}>
2018-08-15 08:25:34 -0500

    //se subio solo el archivo con el codigo

---
+++ semana2/programa2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+	int edad = 0;
+	printf ("insertar tu edad \n >: ");	
+	scanf("%i",&edad);
+	edad+= 10;
+	printf("Tu edad en el futuro sera: %i \n",edad);
+return 0;
+}
