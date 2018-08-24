d05835d822dce7603cfbb968f597c8a24aa43970
leromerol86 <le1110_romero86@outlook.com>
2018-08-24 19:49:16 -0400

    Otro cambio

--- semana3/cartesiano.c
+++ semana3/cartesiano.c
@@ -9,20 +9,19 @@
 		scanf("%f",&y);
 		printf("intorduce el valor de z\n");
 		scanf("%f",&z);
-			r=sqrt(pow(d,2)+pow(y,2)+pow(z,2));
+			r=sqrt(pow(x,2)+pow(y,2)+pow(z,2));
 			y=(180/3.1416)*y;
 			z=(180/3.1416)*z;
 			x=(180/3.1416)*x;
 			a=acos(z/r);
-			p=atan(y/d);
-			d=(3.1416/180)*x;
-			x=(3.1416/180)*y;
+			p=atan(y/x);
+			x=(3.1416/180)*x;
+			y=(3.1416/180)*y;
 			z=(3.1416/180)*z;
-		printf("el valor de r es:%f\n",r);
-		printf("el valor de teta es:%f\n",a);
-		printf("el valor de phi es:%f\n",p);
-		printf("tu coordenada inicial para x fue:%f\n",x);
-		printf("tu coordenada inicial para y fue:%f\n",y);
-		printf("tu coordenada inicial para z fue:%f\n",z);
-				
+			printf("El valor de r es:%f\n",r);
+			printf("El valor de teta es:%f\n",a);
+			printf("El valor de phi es:%f\n",p);
+			printf("Tu coordenada x es:%f\n",x);
+			printf("Tu coordenada y es:%f\n",y);
+			printf("Tu coordenada z es:%f\n",z);
 }
