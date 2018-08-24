42b582fe9bf9f80ec7e100388003308d8d201cd4
leromerol86 <le1110_romero86@outlook.com>
2018-08-24 19:59:08 -0400

    ultima correccion

--- semana3/ifcuatro.c
+++ semana3/ifcuatro.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 	int main (){
 	char respuesta [15];
 	int edad;
-	printf("Selecciona tu genero(escribelo como se muestra): \n Hombre \n Mujer \n");
+	printf("Selecciona tu genero(escribelo como se muestra): \n 1)hombre \n 2)mujer \n");
 	scanf("%s",&respuesta);
 	printf("¿Cual es tu edad? \n");
 	scanf("%i",&edad);			
-		if(respuesta == 'Hombre'){
+		if( strcmp(respuesta,"hombre" )== 0){
 			if(edad <18){
 				printf("no dejes la escuela \n");			
 			}else if(edad > 35){
