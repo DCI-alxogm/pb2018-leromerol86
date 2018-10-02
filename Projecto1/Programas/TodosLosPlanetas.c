36e197f396c288ecc8cb5650061ddd4b996cbb28
leromerol86 <42146601+leromerol86@users.noreply.github.com>
2018-10-01 19:26:59 -0500

    Se agregaron lo programas acabados
    Se agregaron lo programas acabados

--- Projecto1/Programas/TodosLosPlanetas.c
+++ Projecto1/Programas/TodosLosPlanetas.c
@@ -5,59 +5,39 @@
 	int main(){
 	FILE *arc,*re;
 	char dir[255];
-	int c,p[9];
-	printf("Cuantos planetas vas a calcular?");
-	scanf("%i",&c);
+	char Nombre[255];
+	//Se inicia el programa solicitando el nombre del archivo que guarda los datos.
+	int p,rt,c;
 	printf("Escribe el nombre del archivo(sin Extensión)");
 	scanf("%s",dir);
-	double Vx[9],Vy[9],Vz[9],M[9],m[9],t[9],h[9],r[9],x[9],y[9],z[9];
-	strcat(dir,".txt");
-	arc=fopen(dir,"r");
-		for (int i = 0; i <= c; i++) {
-				fscanf(arc,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",&M[i],&m[i],&t[i],&h[i],&x[i],&y[i],&z[i],&Vx[i],&Vy[i],&Vz[i]);
-				p[i] = t[i]/h[i];
-				Vx[i]*=365.242;//Decido a que los valores estan por dia se convierte las unidades de velolcidad a años
-				Vy[i]*=365.242;
-				Vz[i]*=365.242;
-						switch (i) {
-										case 0:
-										re = fopen("uno.txt","w");
-										break;
-										case 1:
-										re = fopen("dos.txt","w");
-										break;
-										case 2:
-										re = fopen("tres.txt","w");
-										break;
-										case 3:
-										re = fopen("cuatro.txt","w");
-										break;
-										case 4:
-										re = fopen("cinco.txt","w");
-										break;
-										case 5:
-										re = fopen("seis.txt","w");
-										break;
-										case 6:
-										re = fopen("siete.txt","w");
-										break;
-										case 7:
-										re = fopen("ocho.txt","w");
-										break;
-										case 8:
-										re = fopen("nueve.txt","w");
-										break;
-				}
-					r[i] = sqrt (pow(x[i],2) + pow(y[i],2) + pow(z[i],2));
-										for (int u = 1 ; u <= p[i]; u++){
-													fprintf(re,"%lf %lf %lf %lf %lf %lf\n",x[i],y[i],z[i],Vx[i],Vy[i],Vz[i]);
-													x[i]+=(Vx[i]*h[i]);
-													y[i]+=(Vy[i]*h[i]);
-													z[i]+=(Vz[i]*h[i]);
-													Vx[i]-= h[i]*((39.4784*M[i]*x[i])/pow(r[i],3));
-													Vy[i]-= h[i]*((39.4784*M[i]*y[i])/pow(r[i],3));
-													Vz[i]-= h[i]*((39.4784*M[i]*z[i])/pow(r[i],3));
-											}
-					fclose(re);
-				}
+	printf("Cuantos planetas vas a calcular?");
+	scanf("%i",&c);
+	printf("Las velocidades de las velocidades estan en \n 1)UA/Años \n 2)UA/Dias\n");
+	scanf("%i",&rt);//Se pregunta en que valor estan las velocidad para cnocer si se necesitan convertir
+	float Vx,Vy,Vz,M,m,t,h,r,x,y,z;
+	strcat(dir,".txt");//Concateno el nombre del archivo con el .txt
+	arc=fopen(dir,"r");//Se abre el archivo para extraer los datos
+			for (int i = 0; i < c; i++) {
+							fscanf(arc,"%s %f %f %f %f %f %f %f %f %f %f",&Nombre,&M,&m,&t,&h,&x,&y,&z,&Vx,&Vy,&Vz);//Se almacenan los datos.
+							p = t/h;
+							if (rt==2) {
+								Vx*=365.242;//Debido a que los valores estan por dia se convierte las unidades de velolcidad a años
+								Vy*=365.242;
+								Vz*=365.242;
+							}
+								re = fopen(Nombre,"w");//abrimos el archivo donde se guardaran los datos
+						//Se realiza un ciclo para la impresión y calculo de los datos
+							for (int u = 1 ; u <= p; u++){
+							r = sqrt (pow(x,2) + pow(y,2) + pow(z,2));
+								fprintf(re,"%f %f %f %f %f %f\n",x,y,z,Vx,Vy,Vz);//Se imprimen los primeros valores, que son los que se exrajeron del primer archivo
+						//Desplazamiento de las coordenadas
+								x+=(Vx*h);
+								y+=(Vy*h);
+								z+=(Vz*h);
+						//Cambio de la velolcidad
+								Vx-= h*((39.4784*M*x)/pow(r,3));
+								Vy-= h*((39.4784*M*y)/pow(r,3));
+								Vz-= h*((39.4784*M*z)/pow(r,3));
+							}
+							}
 }
--- Projecto1/Programas/UnPlaneta.c
+++ Projecto1/Programas/UnPlaneta.c
@@ -5,20 +5,25 @@
 	int main(){
 	FILE *arc,*re;
 	char dir[255];
+	char Nombre[255];
 	//Se inicia el programa solicitando el nombre del archivo que guarda los datos.
+	int p,rt;
 	printf("Escribe el nombre del archivo(sin Extensión)");
 	scanf("%s",dir);
+	printf("Las velocidades de las velocidades estan en \n 1)UA/Años \n 2)UA/Dias");
+	scanf("%i",&rt);//Se pregunta en que valor estan las velocidad para cnocer si se necesitan convertir
 	float Vx,Vy,Vz,M,m,t,h,r,x,y,z;
-	int p;
 	strcat(dir,".txt");//Concateno el nombre del archivo con el .txt
 	arc=fopen(dir,"r");//Se abre el archivo para extraer los datos
-	fscanf(arc,"%f %f %f %f %f %f %f %f %f %f",&M,&m,&t,&h,&x,&y,&z,&Vx,&Vy,&Vz);//Se almacenan los datos.
+	fscanf(arc,"%s %f %f %f %f %f %f %f %f %f %f",&Nombre,&M,&m,&t,&h,&x,&y,&z,&Vx,&Vy,&Vz);//Se almacenan los datos.
 	fclose(arc);
 	p = t/h;
-	Vx*=365.242;//Debido a que los valores estan por dia se convierte las unidades de velolcidad a años
-	Vy*=365.242;
-	Vz*=365.242;
-	re = fopen("respuesta.txt","w");//abrimos el archivo donde se guardaran los datos
+	if (rt==2) {
+		Vx*=365.242;//Debido a que los valores estan por dia se convierte las unidades de velolcidad a años
+		Vy*=365.242;
+		Vz*=365.242;
+	}
+		re = fopen(Nombre,"w");//abrimos el archivo donde se guardaran los datos
 //Se realiza un ciclo para la impresión y calculo de los datos
 	for (int u = 1 ; u <= p; u++){
 	r = sqrt (pow(x,2) + pow(y,2) + pow(z,2));
