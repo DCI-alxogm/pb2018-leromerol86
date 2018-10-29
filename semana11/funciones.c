339bc33013b0fdc28f35e2606e716d93eb8cee38
luis <luis@localhost.localdomain>
2018-10-28 23:59:07 -0600

    Avance

--- semana11/funciones.c
+++ semana11/funciones.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void PuntosV()
+float PuntosV(float v[4])
 	{
-		
-
+		for (int i = 0; i < 4; i++) {
+			h+=V[i];
+		}
+		h/=4;
+		return h;
 	}
